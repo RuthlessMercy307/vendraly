90b791d6344366d2feb39579d2e6106625bdce85
RuthlessMercy307 <ruthlessmercy307@gmail.com>
2025-06-22 14:11:48 -0300

    nada

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '01312003',
-  database: process.env.DB_NAME || 'vendraly'
-};
